5c10efaef6080d7ff7517a39319bbe539dce54cc
Natdenice <Clad1987@hotmail.com>
2019-01-22 20:23:05 +0100

    basic calculator added for hpb + starting to make responsive

--- crypto.js
+++ crypto.js
@@ -1,19 +1,24 @@
+var portfolioAmount = '';
+var hpbPrice = '';
+
 var input = document.querySelectorAll('input');
-console.log(input);
 
 for (var i = 0; i < input.length; i++) {
-  console.log(input[i]);
-  input[i].addEventListener('click', function() {
-    alert('connected');
+  input[i].addEventListener('click', function() { 
+
   })
 };
 
+var result = document.querySelector('#resultParagraph');
+var button = document.querySelector('button');
+button.addEventListener('click', function() {
+  portfolioAmount =  (input[0].value / input[1].value)*input[2].value;
+  hpbPrice = (input[0].value / input[1].value);
+  result.textContent = 'Your hpb portfolio will worth' + ' ' + portfolioAmount + ' ' + 'USD.' + ' ' +
+  'With the Marketcap and the Circulation Supply specified, HPB Token will worth' + ' ' + hpbPrice 
+  + ' ' + 'USD';
+});
 
-function calculatePrice(price, quantity) {
-  return price * quantity;
-};
-
-console.log(calculatePrice(100,1000));
 
 
 

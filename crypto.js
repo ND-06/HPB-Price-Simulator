9fde408f6dff1d0fceadb943be241e3513013ba9
Natdenice <Clad1987@hotmail.com>
2019-01-22 20:34:18 +0100

    variables assigned to the three main inputs in Js

--- crypto.js
+++ crypto.js
@@ -1,19 +1,22 @@
 var portfolioAmount = '';
 var hpbPrice = '';
+var marketCap = '';
+var circulationSupply = '';
+var tokenQuantity = '';
 
 var input = document.querySelectorAll('input');
 
 for (var i = 0; i < input.length; i++) {
-  input[i].addEventListener('click', function() { 
-
-  })
+  marketCap = input[0];
+  circulationSupply = input[1];
+  tokenQuantity = input[2]; 
 };
 
 var result = document.querySelector('#resultParagraph');
 var button = document.querySelector('button');
 button.addEventListener('click', function() {
-  portfolioAmount =  (input[0].value / input[1].value)*input[2].value;
-  hpbPrice = (input[0].value / input[1].value);
+  portfolioAmount =  (marketCap.value / circulationSupply.value)*tokenQuantity.value;
+  hpbPrice = (marketCap.value / circulationSupply.value);
   result.textContent = 'Your hpb portfolio will worth' + ' ' + portfolioAmount + ' ' + 'USD.' + ' ' +
   'With the Marketcap and the Circulation Supply specified, HPB Token will worth' + ' ' + hpbPrice 
   + ' ' + 'USD';

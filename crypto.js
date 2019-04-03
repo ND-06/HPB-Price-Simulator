c10f9870863a8061eff0b6e25510ab6f6dce7179
Nikita Vasilev <vassilev.nikita@gmail.com>
2019-04-03 19:01:24 +0200

    Promise Object fix + ES6 + formatNumber ft

--- crypto.js
+++ crypto.js
@@ -1,9 +1,24 @@
-var portfolioAmount = '';
-var hpbPrice = '';
-var marketCap = '';
-var circulationSupply = '';
-var tokenQuantity = '';
+let portfolioAmount, hpbPrice, marketCap, circulationSupply, tokenQuantity;
 const coinInfoParagraph = document.querySelector('#cryptoinfo');
+
+const formatNumber = num => {
+  let numSplit, int, dec;
+
+  num = Math.abs(num);
+  num = num.toFixed(2);
+  numSplit = num.split('.');
+
+  int = numSplit[0];
+
+  if (int.length > 3) {
+    for (let i = 3; i < int.length; i += 4) {
+      int = int.substr(0, int.length - i) + ',' + int.substr(int.length - i, i);
+    }
+  }  
+  dec = numSplit[1];
+
+  return `${int}.${dec}`;
+};
 
 async function getPriceAndMcap() {
   try {
@@ -12,35 +27,36 @@ async function getPriceAndMcap() {
     const data = await result.json();
     const price = data.market_data.current_price.usd;
     const marketCap = data.market_data.market_cap.usd;
-    let coinInfo = `The current price of High Performance Blockchain is ${price}$ with a current market Cap of ${marketCap}$`;
-    const newContent = `<p class='text-info text-center mt-5' id='cryptoinfo'>${coinInfo}</p>`;
-    coinInfoParagraph.insertAdjacentHTML('afterbegin', newContent);
+    const coinInfo = `The current price of High Performance Blockchain is $${formatNumber(price)} with a current market Cap of $${formatNumber(marketCap)}`;
+    return coinInfo;
   } catch(error) {
     console.log(error);
   }
 }
 
-function simulateFuturePrice () {
-  var input = document.querySelectorAll('input');
+getPriceAndMcap().then(result => {
+  const newContent = `<p class='text-info text-center mt-5' id='cryptoinfo'>${result}</p>`;
+  coinInfoParagraph.insertAdjacentHTML('afterbegin', newContent);
+});
 
-for (var i = 0; i < input.length; i++) {
-  marketCap = input[0];
-  circulationSupply = input[1];
-  tokenQuantity = input[2]; 
-};
+function simulateFuturePrice() {
+  const input = document.querySelectorAll('input');
 
-var result = document.querySelector('#resultParagraph');
-var button = document.querySelector('button');
-button.addEventListener('click', function() {
-  portfolioAmount =  (marketCap.value / circulationSupply.value)*tokenQuantity.value;
-  hpbPrice = (marketCap.value / circulationSupply.value);
-  result.textContent = 'Your hpb portfolio will worth' + ' ' + portfolioAmount + ' ' + 'USD.' + ' ' +
-  'With the Marketcap and the Circulation Supply specified, HPB Token will worth' + ' ' + hpbPrice 
-  + ' ' + 'USD';
-})
+  for (let i = 0; i < input.length; i++) {
+    marketCap = input[0];
+    circulationSupply = input[1];
+    tokenQuantity = input[2]; 
+  }
+
+  const result = document.querySelector('#resultParagraph');
+  const button = document.querySelector('button');
+  button.addEventListener('click', function() {
+    portfolioAmount = (marketCap.value / circulationSupply.value)*tokenQuantity.value;
+    hpbPrice = (marketCap.value / circulationSupply.value);
+    result.textContent = `
+      Your hpb portfolio will worth ${formatNumber(portfolioAmount)} USD. With the Marketcap and the Circulation Supply specified, HPB Token will worth ${formatNumber(hpbPrice)} USD
+    `;
+  });
 }
 
-getPriceAndMcap();
 simulateFuturePrice();
-
-

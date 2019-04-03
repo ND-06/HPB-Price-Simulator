62acffe9ee4c5734723c61c45cb644e81b8cc0b8
Natdenice <Clad1987@hotmail.com>
2019-04-03 18:06:38 +0200

    Api CoinGecko connected and getPriceAndMcap async function created

--- crypto.js
+++ crypto.js
@@ -3,8 +3,25 @@ var hpbPrice = '';
 var marketCap = '';
 var circulationSupply = '';
 var tokenQuantity = '';
+const coinInfoParagraph = document.querySelector('#cryptoinfo');
 
-var input = document.querySelectorAll('input');
+async function getPriceAndMcap() {
+  try {
+    const result = await fetch 
+    (`https://api.coingecko.com/api/v3/coins/high-performance-blockchain`);
+    const data = await result.json();
+    const price = data.market_data.current_price.usd;
+    const marketCap = data.market_data.market_cap.usd;
+    let coinInfo = `The current price of High Performance Blockchain is ${price}$ with a current market Cap of ${marketCap}$`;
+    const newContent = `<p class='text-info text-center mt-5' id='cryptoinfo'>${coinInfo}</p>`;
+    coinInfoParagraph.insertAdjacentHTML('afterbegin', newContent);
+  } catch(error) {
+    console.log(error);
+  }
+}
+
+function simulateFuturePrice () {
+  var input = document.querySelectorAll('input');
 
 for (var i = 0; i < input.length; i++) {
   marketCap = input[0];
@@ -20,11 +37,10 @@ button.addEventListener('click', function() {
   result.textContent = 'Your hpb portfolio will worth' + ' ' + portfolioAmount + ' ' + 'USD.' + ' ' +
   'With the Marketcap and the Circulation Supply specified, HPB Token will worth' + ' ' + hpbPrice 
   + ' ' + 'USD';
-});
+})
+}
 
-
-
-
-
+getPriceAndMcap();
+simulateFuturePrice();
 
 

84bca2e7793c2c0774b19f4285f674d7117e8fed
Natdenice <Clad1987@hotmail.com>
2019-04-08 16:35:30 +0200

    Test for eslint

--- crypto.js
+++ crypto.js
@@ -1,4 +1,11 @@
-let portfolioAmount, hpbFuturePrice, marketCap, circulatingSupply, tokenQuantity, button, result, coinInfoParagraph;
+let portfolioAmount;
+let hpbFuturePrice;
+let marketCap;
+let circulatingSupply;
+let tokenQuantity;
+let button;
+let result;
+let coinInfoParagraph;
 
 marketCap = document.getElementById('marketcapInput');
 circulatingSupply = document.getElementById('circulatingSupplyInput');
@@ -6,8 +13,10 @@ tokenQuantity = document.getElementById('tokenquantityInput');
 result = document.querySelector('#resultParagraph');
 coinInfoParagraph = document.querySelector('#cryptoinfo');
 
-const formatNumber = num => {
-  let numSplit, int, dec;
+const formatNumber = (num) => {
+  let numSplit;
+  let int;
+  let dec;
 
   num = Math.abs(num);
   num = num.toFixed(2);
@@ -17,9 +26,9 @@ const formatNumber = num => {
 
   if (int.length > 3) {
     for (let i = 3; i < int.length; i += 4) {
-      int = int.substr(0, int.length - i) + ',' + int.substr(int.length - i, i);
+      int = `${int.substr(0, int.length - i)},${int.substr(int.length - i, i)}`;
     }
-  }  
+  }
   dec = numSplit[1];
 
   return `${int}.${dec}`;
@@ -27,44 +36,50 @@ const formatNumber = num => {
 
 async function getPriceAndMcap() {
   try {
-    const result = await fetch 
-    (`https://api.coingecko.com/api/v3/coins/high-performance-blockchain`);
+    const result = await fetch(
+      'https://api.coingecko.com/api/v3/coins/high-performance-blockchain',
+    );
     const data = await result.json();
     const currentCirculatingSupply = data.market_data.circulating_supply;
     const price = data.market_data.current_price.usd;
     const marketCap = data.market_data.market_cap.usd;
-    const coinInfo = `The current price of High Performance Blockchain is $${formatNumber(price)} with a current market Cap of $${formatNumber(marketCap)} and a current circulating supply of ${formatNumber(currentCirculatingSupply)} tokens.`;
+    const coinInfo = `The current price of High Performance Blockchain is $${formatNumber(
+      price,
+    )} with a current market Cap of $${formatNumber(
+      marketCap,
+    )} and a current circulating supply of ${formatNumber(currentCirculatingSupply)} tokens.`;
     return coinInfo;
-  } catch(error) {
+  } catch (error) {
     console.log(error);
   }
 }
 
-getPriceAndMcap().then(result => {
+getPriceAndMcap().then((result) => {
   const newContent = `<p class='text-info text-center mt-5' id='cryptoinfo'>${result}</p>`;
   coinInfoParagraph.insertAdjacentHTML('afterbegin', newContent);
 });
 
 function simulateFuturePrice() {
-    portfolioAmount = (marketCap.value / circulatingSupply.value)*tokenQuantity.value;
-    hpbFuturePrice = (marketCap.value / circulatingSupply.value);
-    result.textContent = `
-      Your hpb portfolio will worth $${formatNumber(portfolioAmount)} USD. With the Marketcap and the Circulating Supply specified, HPB Token will worth ${formatNumber(hpbFuturePrice)} USD
+  portfolioAmount = (marketCap.value / circulatingSupply.value) * tokenQuantity.value;
+  hpbFuturePrice = marketCap.value / circulatingSupply.value;
+  result.textContent = `
+      Your hpb portfolio will worth $${formatNumber(
+    portfolioAmount,
+  )} USD. With the Marketcap and the Circulating Supply specified, HPB Token will worth ${formatNumber(
+  hpbFuturePrice,
+)} USD
     `;
-};
+}
 
 const setupEventListeners = () => {
-  document.addEventListener('keypress', function(event){ 
+  document.addEventListener('keypress', (event) => {
     if (event.keyCode === 13 || event.which === 13) {
       simulateFuturePrice();
       event.preventDefault();
     }
-  });  
+  });
   button = document.querySelector('button');
   button.addEventListener('click', simulateFuturePrice);
 };
 
 setupEventListeners();
-
-
-let arr = 
---
+++ test.js
@@ -0,0 +1,6 @@
+const jiji = [54, 54, 54, 54, 54, 54];
+
+const modifyJiji = (arr) => {
+  const x = [...arr];
+  x.push(arr);
+};

70faaea75db36692b9818e1488284d8aea5508a8
Natdenice <Clad1987@hotmail.com>
2019-04-09 14:47:42 +0200

    minor syntax errors fixed according to eslint & prettier rules

--- crypto.js
+++ crypto.js
@@ -1,46 +1,37 @@
 let portfolioAmount;
 let hpbFuturePrice;
-let marketCap;
-let circulatingSupply;
-let tokenQuantity;
 let button;
-let resultInfo;
-let coinInfoParagraph;
 
-marketCap = document.getElementById('marketcapInput');
-circulatingSupply = document.getElementById('circulatingSupplyInput');
-tokenQuantity = document.getElementById('tokenquantityInput');
-result = document.querySelector('#resultParagraph');
-coinInfoParagraph = document.querySelector('#cryptoinfo');
+const marketCap = document.getElementById('marketcapInput');
+const circulatingSupply = document.getElementById('circulatingSupplyInput');
+const tokenQuantity = document.getElementById('tokenquantityInput');
+const result = document.querySelector('#resultParagraph');
+const coinInfoParagraph = document.querySelector('#cryptoinfo');
 
 const formatNumber = (num) => {
-  let numSplit;
-  let int;
-  let dec;
-
+  // eslint-disable-next-line no-param-reassign
   num = Math.abs(num);
+  // eslint-disable-next-line no-param-reassign
   num = num.toFixed(2);
-  numSplit = num.split('.');
-
-  int = numSplit[0];
-
+  const numSplit = num.split('.');
+  // eslint-disable-next-line prefer-destructuring
+  let int = numSplit[0];
   if (int.length > 3) {
     for (let i = 3; i < int.length; i += 4) {
       int = `${int.substr(0, int.length - i)},${int.substr(int.length - i, i)}`;
     }
   }
-  dec = numSplit[1];
-
+  const dec = numSplit[1];
   return `${int}.${dec}`;
 };
 
 function refresh() {
   async function getPriceAndMcap() {
     try {
-      const result = await fetch(
+      const dataResult = await fetch(
         'https://api.coingecko.com/api/v3/coins/high-performance-blockchain',
       );
-      const data = await result.json();
+      const data = await dataResult.json();
       const currentCirculatingSupply = data.market_data.circulating_supply;
       const price = data.market_data.current_price.usd;
       const marketCapInfo = data.market_data.market_cap.usd;
@@ -51,11 +42,12 @@ function refresh() {
       )} and a current circulating supply of ${formatNumber(currentCirculatingSupply)} tokens.`;
       return coinInfo;
     } catch (error) {
-      console.log(error);
+      return error;
     }
   }
-  getPriceAndMcap().then((resultInfo) => {
-    const newContent = `<p class='text-info text-center mt-5' id='cryptoinfo'>${resultInfo}</p>`;
+  // eslint-disable-next-line no-shadow
+  getPriceAndMcap().then((result) => {
+    const newContent = `<p class='text-info text-center mt-5' id='cryptoinfo'>${result}</p>`;
     coinInfoParagraph.insertAdjacentHTML('afterbegin', newContent);
   });
 }
@@ -86,6 +78,6 @@ const setupEventListeners = () => {
 setupEventListeners();
 
 window.setInterval(() => {
+  coinInfoParagraph.innerHTML = '';
   refresh();
-  console.log('Hello');
-}, 6000);
+}, 2000);
